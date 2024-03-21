af07a25a9d19aa975d98b253770cdc362b1ab601
Eding Muhamad Saprudin <masaruedo@yahoo.com>
2024-03-21 08:03:47 +0700

    Add a feature to edit existing products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -59,7 +59,10 @@ class ProductController extends Controller
      */
     public function edit(Product $product)
     {
-        //
+        return inertia('Product/Edit', [
+            'categories' => CategoryResource::collection(Category::orderBy('name')->get()),
+            'product' => ProductResource::make($product)
+        ]);
     }
 
     /**
@@ -67,7 +70,9 @@ class ProductController extends Controller
      */
     public function update(UpdateProductRequest $request, Product $product)
     {
-        //
+        $product->update($request->validated());
+
+        return redirect()->route('products.index');
     }
 
     /**
