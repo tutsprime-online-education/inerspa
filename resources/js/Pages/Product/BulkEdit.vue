cbdf2cd52da864a9d335d3d191709e30f309bdf2
Eding Muhamad Saprudin <masaruedo@yahoo.com>
2024-03-21 09:51:58 +0700

    Add a form modal to update multiple products

---
+++ resources/js/Pages/Product/BulkEdit.vue
@@ -0,0 +1,64 @@
+<script setup>
+import Modal from '@/Components/Modal.vue';
+const props = defineProps({
+    show: {
+        type: Boolean,
+        default: false
+    }
+})
+
+const emit = defineEmits(['close'])
+</script>
+<template>
+    <Modal :show="show" max-width="xl" @close="emit('close')">
+        <form class="relative bg-white rounded-lg shadow">
+            <!-- Modal header -->
+            <div class="flex items-start justify-between p-4 border-b rounded-t">
+                <h3 class="text-xl font-semibold text-gray-900">
+                    Bulk Edit
+                </h3>
+                <button @click.prevent="emit('close')" type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="editUserModal">
+                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                    </svg>
+                    <span class="sr-only">Close modal</span>
+                </button>
+            </div>
+            <!-- Modal body -->
+            <div class="p-6 space-y-6">
+                <div class="grid grid-cols-6 gap-6">
+                    <div class="col-span-6 sm:col-span-6">
+                        <label class="block mb-2 text-sm font-medium text-gray-900 ">Products</label>
+                        <ul>
+                            <li class="flex items-center">
+                                <svg class="w-3.5 h-3.5 me-2 text-cyan-500 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+                                </svg>
+                                At least one lowercase character
+                            </li>
+                            <li class="flex items-center">
+                                <svg class="w-3.5 h-3.5 me-2 text-cyan-500 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+                                </svg>
+                                At least one lowercase character
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-span-6 sm:col-span-6">
+                        <label for="category_id" class="block mb-2 text-sm font-medium text-gray-900 ">Category</label>
+                        <select name="category_id" id="category_id" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5" required="">
+                            <option value="">Category 1</option>
+                            <option value="">Category 2</option>
+                            <option value="">Category 3</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <!-- Modal footer -->
+            <div class="text-right p-6 space-x-2 border-t rounded-b">
+                <button @click.prevent="emit('close')" type="button" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5">Cancel</button>
+                <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Update</button>
+            </div>
+        </form>
+    </Modal>
+</template>
--- resources/js/Pages/Product/Index.vue
+++ resources/js/Pages/Product/Index.vue
@@ -5,9 +5,11 @@ import Pagination from '@/Components/Pagination.vue';
 import Sortable from '@/Components/Sortable.vue';
 import Checkbox from '@/Components/Checkbox.vue';
 import CheckAll from '@/Components/CheckAll.vue';
+import BulkEdit from './BulkEdit.vue';
 import { Head, Link, router } from '@inertiajs/vue3';
 
 const selectedIds = ref([])
+const showModal = ref(false)
 
 const deleteRow = (id) => {
     if (window.confirm("Are you sure?")) {
@@ -60,13 +62,22 @@ const handleSearch = (event) => {
         <div class="py-6">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex flex-column sm:flex-row flex-wrap space-y-4 sm:space-y-0 items-center justify-between pb-6">
-                    <button type="button" 
-                        class="px-3 py-2.5 text-sm font-medium text-center text-white rounded-md" 
-                        :class="{ 'bg-red-300 cursor-not-allowed': !selectedIds.length, 'bg-red-500': selectedIds.length }"
-                        :disabled="!selectedIds.length"
-                        @click="deleteSelected"
-                    >
-                        Delete Selected</button>
+                    <div class="space-x-3">
+                        <button type="button" 
+                            class="px-3 py-2.5 text-sm font-medium text-center text-white rounded-md" 
+                            :class="{ 'bg-red-300 cursor-not-allowed': !selectedIds.length, 'bg-red-500': selectedIds.length }"
+                            :disabled="!selectedIds.length"
+                            @click="deleteSelected"
+                        >
+                            Delete Selected</button>
+                        <button type="button"
+                            class="px-3 py-2.5 text-sm font-medium text-center text-white rounded-md"
+                            :class="{ 'bg-cyan-300 cursor-not-allowed': !selectedIds.length, 'bg-cyan-500': selectedIds.length }"
+                            :disabled="!selectedIds.length"
+                            @click="showModal = true"
+                        >
+                            Edit Selected</button>
+                    </div>
                     <div class="relative">
                         <div class="absolute inset-y-0 left-0 rtl:inset-r-0 rtl:right-0 flex items-center ps-3 pointer-events-none">
                             <svg class="w-5 h-5 text-gray-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
@@ -133,5 +144,6 @@ const handleSearch = (event) => {
                 </div>
             </div>
         </div>
+        <BulkEdit :show="showModal" @close="showModal = false" />
     </AuthenticatedLayout>
 </template>
