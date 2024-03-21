29b2ccd9088fc71c72a13b4305afeb342d3a74c0
Eding Muhamad Saprudin <masaruedo@yahoo.com>
2024-03-21 09:28:53 +0700

    Create pagination component

--- resources/js/Pages/Product/Index.vue
+++ resources/js/Pages/Product/Index.vue
@@ -1,5 +1,6 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import Pagination from '@/Components/Pagination.vue';
 import { Head, Link, router } from '@inertiajs/vue3';
 
 const deleteRow = (id) => {
@@ -12,7 +13,7 @@ const deleteRow = (id) => {
 
 defineProps({
     products: {
-        type: Array,
+        type: Object,
         required: true
     }
 })
@@ -35,6 +36,9 @@ defineProps({
                     <table class="w-full text-sm text-left rtl:text-right text-gray-500">
                         <thead class="text-xs text-gray-700 uppercase bg-gray-50 border-b">
                             <tr>
+                                <th scope="col" class="px-6 py-3" width="5">
+                                    No
+                                </th>
                                 <th scope="col" class="px-6 py-3">
                                     Product name
                                 </th>
@@ -53,7 +57,10 @@ defineProps({
                             </tr>
                         </thead>
                         <tbody>
-                            <tr v-for="product in products" :key="product.id" class="bg-white border-b hover:bg-gray-50">
+                            <tr v-for="(product, index) in products.data" :key="product.id" class="bg-white border-b hover:bg-gray-50">
+                                <td class="px-6 py-4">
+                                    {{ products.meta.from + index }}
+                                </td>
                                 <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
                                     {{ product.name }}
                                 </th>
@@ -74,32 +81,7 @@ defineProps({
                             </tr>
                         </tbody>
                     </table>
-                    <nav class="flex items-center flex-column flex-wrap md:flex-row justify-between py-2 px-4" aria-label="Table navigation">
-                        <span class="text-sm font-normal text-gray-700 mb-4 md:mb-0 block w-full md:inline md:w-auto">Showing <span class="font-semibold text-gray-700">1-10</span> of <span class="font-semibold text-gray-700">1000</span></span>
-                        <ul class="inline-flex -space-x-px rtl:space-x-reverse text-sm h-8">
-                            <li>
-                                <a href="#" class="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 hover:text-gray-700">Previous</a>
-                            </li>
-                            <li>
-                                <a href="#" class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 hover:text-gray-700">1</a>
-                            </li>
-                            <li>
-                                <a href="#" class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 hover:text-gray-700">2</a>
-                            </li>
-                            <li>
-                                <a href="#" aria-current="page" class="flex items-center justify-center px-3 h-8 text-blue-600 hover:text-blue-700">3</a>
-                            </li>
-                            <li>
-                                <a href="#" class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 hover:text-gray-700">4</a>
-                            </li>
-                            <li>
-                                <a href="#" class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 hover:text-gray-700">5</a>
-                            </li>
-                            <li>
-                        <a href="#" class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 hover:text-gray-700">Next</a>
-                            </li>
-                        </ul>
-                    </nav>
+                    <Pagination :meta="products.meta" />
                 </div>
             </div>
         </div>
