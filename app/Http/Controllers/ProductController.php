6d2d48425d8b043c21d61858e898b0942e09b9c3
Eding Muhamad Saprudin <masaruedo@yahoo.com>
2024-03-21 09:39:40 +0700

    Handle sorting functionality on the server side

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -19,15 +19,23 @@ class ProductController extends Controller
     {
         $products = auth()->user()
             ->products()
-            ->latest()
             ->with('category')
             ->where(function ($query) {
                 if ($search = request()->search) {
                     $query->where('name', 'like', '%' . $search . '%')
-                        ->orWhereHas('category', function ($query) use ($search) {
-                            $query->where('name', 'like', '%' . $search . '%');
-                        });
+                    ->orWhereHas('category', function ($query) use ($search) {
+                        $query->where('name', 'like', '%' . $search . '%');
+                    });
                 }
+            })
+            ->when(!request()->query('sort_by'), function ($query) {
+                $query->latest();
+            })
+            ->when(in_array(request()->query('sort_by'), ['name', 'price', 'weight']), function ($query) {
+                $sortBy = request()->query('sort_by');
+                $field = ltrim($sortBy, '-');
+                $direction = substr($sortBy, 0, 1) === '-' ? 'desc' : 'asc';
+                $query->orderBy($field, $direction);
             })
             ->paginate(10)
             ->withQueryString();
