a889cee4b3c2f9d304a16ec2c4da6adb1632d1ed
Eding Muhamad Saprudin <masaruedo@yahoo.com>
2024-03-21 09:33:40 +0700

    Add a search form

--- resources/js/Pages/Product/Index.vue
+++ resources/js/Pages/Product/Index.vue
@@ -11,12 +11,24 @@ const deleteRow = (id) => {
     }
 }
 
-defineProps({
+const props = defineProps({
     products: {
         type: Object,
         required: true
+    },
+    query: {
+        type: Object,
+        default: () => ({
+            search: ''
+        })
     }
 })
+
+const handleSearch = (event) => {
+    router.get(route('products.index'), {
+        search: event.target.value
+    })
+}
 </script>
 
 <template>
@@ -30,8 +42,16 @@ defineProps({
             </div>
         </template>
 
-        <div class="py-12">
+        <div class="py-6">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="flex flex-column sm:flex-row flex-wrap space-y-4 sm:space-y-0 items-center justify-end pb-6">
+                    <div class="relative">
+                        <div class="absolute inset-y-0 left-0 rtl:inset-r-0 rtl:right-0 flex items-center ps-3 pointer-events-none">
+                            <svg class="w-5 h-5 text-gray-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+                        </div>
+                        <input type="text" id="table-search" @keydown.enter="handleSearch" :value="props.query.search" class="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500" placeholder="Search for items">
+                    </div>
+                </div>
                 <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
                     <table class="w-full text-sm text-left rtl:text-right text-gray-500">
                         <thead class="text-xs text-gray-700 uppercase bg-gray-50 border-b">
